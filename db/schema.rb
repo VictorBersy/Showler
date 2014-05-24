d17767f51d262882b377c483c7cf20e641462ea8
Victor Bersy <victorbersy+github@gmail.com>
2014-05-24 02:17:47 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523184349) do
+ActiveRecord::Schema.define(version: 20140524001142) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "screen_name"
     t.string   "password_digest"
+    t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
